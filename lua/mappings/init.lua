c90c4ba568ccca74987e01551a3c03265b658f0f
Kevin Ngo <kevinngo.cal@gmail.com>
2022-06-19 00:21:46 -0700

    add nvim-tree config and mappings

---
+++ lua/mappings/init.lua
@@ -0,0 +1,8 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Set leader key as space
+vim.g.mapleader = ' '
+
+-- NvimTree 
+map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
